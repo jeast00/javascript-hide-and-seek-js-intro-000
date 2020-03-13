4e5cf2ba1ac21e55dff81ffa60fe2ce20805f43f
Jonathan East <jonathan_east@ymail.com>
2020-03-13 01:38:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
 
+function nestedTarget() {
+ return document.querySelector("#nested .target");
+}
+
+function deepestChild() {
+  return document.querySelector('#grand-node div div div div');
+}
+
+function increaseRankBy(n) {
+  const rankedList = document.querySelectorAll('.rankedList');
+
+}
